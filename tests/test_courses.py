25e42395bc6074b48d9e1bbe85818b0fcac4b2b1
Sergey Tyurin <viralbans@yandex.ru>
2025-09-16 21:01:28 +0500

    Практикуемся в реализации PageObject.Практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -38,7 +38,7 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
     create_course_page.check_visible_create_exercise_button()
     create_course_page.check_visible_exercises_empty_view()
     create_course_page.upload_preview_image('./testdata/files/image.png')
-    create_course_page.check_visible_image_upload_view()
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
     create_course_page.fill_create_course_form(title='Playwright',estimated_time='2 weeks',description='Playwright',max_score='100',min_score='10')
     create_course_page.click_create_course_button()
 
